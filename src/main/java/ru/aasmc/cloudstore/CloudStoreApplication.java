05c4cdb69befb5e4038eb46b33bd38a04e7b7033
Alex Alexeyev <aasmc@yandex.ru>
2022-09-11 02:05:06 +0300

    Add rate limit via Bucket4J

--- src/main/java/ru/aasmc/cloudstore/CloudStoreApplication.java
+++ src/main/java/ru/aasmc/cloudstore/CloudStoreApplication.java
@@ -2,7 +2,9 @@ package ru.aasmc.cloudstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class CloudStoreApplication {
 
